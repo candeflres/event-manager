41bb9ca03ab42f6358126158f51da35f8322f841
gero-jerpi <gero.jerpi50@gmail.com>
2026-01-18 18:01:15 -0300

    add html and styles

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Home } from './components/home/home';
+import { KnowMore } from './components/know-more/know-more';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Home, KnowMore],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ frontend/src/app/components/know-more/know-more.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-know-more',
+  imports: [],
+  templateUrl: './know-more.html',
+  styleUrl: './know-more.css',
+})
+export class KnowMore {
+
+}
