638730d3a157950fd76ea0eb57429bd7cfa74b45
candeflres <soflor51402@gmail.com>
2026-01-22 00:07:39 -0300

    footer

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { KnowMore } from './know-more/know-more';
 import { Hero } from './hero/hero';
 import { PastEvents } from './past-events/past-events';
+import { Footer } from '../../components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [KnowMore, Hero, PastEvents],
+  imports: [KnowMore, Hero, PastEvents, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
