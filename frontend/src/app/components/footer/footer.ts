3bdb6f681373df0942cf032fd2abe5d16d16f406
candeflres <soflor51402@gmail.com>
2026-01-22 02:17:34 -0300

    header and footer fixed

--- frontend/src/app/components/footer/footer.ts
+++ frontend/src/app/components/footer/footer.ts
@@ -1,11 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
-
-}
+export class Footer {}
