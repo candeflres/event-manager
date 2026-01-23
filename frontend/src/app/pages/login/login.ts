5a8bc9eb8ca902175648d36a70fab55edacb8f7c
candeflres <soflor51402@gmail.com>
2026-01-23 17:28:07 -0300

    conección login backend + frontend

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, RouterModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
+
+  login() {
+    const headers = new HttpHeaders({
+      Authorization: 'Basic ' + btoa(`${this.email}:${this.password}`),
+    });
+
+    this.http.get('http://localhost:8080/api/users', { headers }).subscribe({
+      next: () => {
+        this.router.navigate(['/home-logged']);
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error de login');
+      },
+    });
+  }
+}
