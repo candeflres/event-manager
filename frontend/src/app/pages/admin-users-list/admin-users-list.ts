e4cdaa525775f36e8660176df63fb43564119c0a
candeflres <soflor51402@gmail.com>
2026-02-02 22:35:05 -0300

    admin filter users

--- frontend/src/app/pages/admin-users-list/admin-users-list.ts
+++ frontend/src/app/pages/admin-users-list/admin-users-list.ts
@@ -3,15 +3,20 @@ import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserProfile } from '../../model/userProfile';
 import { UserService } from '../../services/user-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './admin-users-list.html',
   styleUrls: ['./admin-users-list.css'],
 })
 export class AdminUsersList implements OnInit {
   users: UserProfile[] = [];
+  filteredUsers: UserProfile[] = [];
+
+  roleFilter: '' | 'CLIENT' | 'EMPLOYEE' | 'ADMIN' = '';
+  statusFilter: '' | 'active' | 'inactive' = '';
 
   constructor(
     private userService: UserService,
@@ -21,7 +26,21 @@ export class AdminUsersList implements OnInit {
   ngOnInit(): void {
     this.userService.getAllUsersIncludingInactive().subscribe((res) => {
       this.users = res;
+      this.applyFilters();
       this.cdr.detectChanges();
+    });
+  }
+
+  applyFilters(): void {
+    this.filteredUsers = this.users.filter((u) => {
+      const roleOk = !this.roleFilter || u.role === this.roleFilter;
+
+      const statusOk =
+        !this.statusFilter ||
+        (this.statusFilter === 'active' && u.active) ||
+        (this.statusFilter === 'inactive' && !u.active);
+
+      return roleOk && statusOk;
     });
   }
 }
