01893f7c6bdc8363a3598c52f01a89c4f9f1a173
candeflres <soflor51402@gmail.com>
2026-02-03 23:23:59 -0300

    landing frontend fixed

--- frontend/src/app/components/header/header.ts
+++ frontend/src/app/components/header/header.ts
@@ -11,12 +11,26 @@ import { AuthService } from '../../services/auth-service';
   styleUrls: ['./header.css'],
 })
 export class Header {
+  menuOpen = false;
+
   constructor(
     private router: Router,
     private authService: AuthService,
   ) {}
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.classList.toggle('menu-open', this.menuOpen);
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.classList.remove('menu-open');
+  }
+
   goHome() {
+    this.closeMenu();
+
     if (this.authService.isLoggedIn()) {
       this.router.navigate(['/home-logged']);
     } else {
@@ -25,6 +39,8 @@ export class Header {
   }
 
   goOrLogin(path: string) {
+    this.closeMenu();
+
     if (this.authService.isLoggedIn()) {
       this.router.navigate([path]);
     } else {
